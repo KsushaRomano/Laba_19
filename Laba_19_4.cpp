adccda364a4b1a9d7b4ae9c506bb4ab0c71f6d0e
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-10-21 14:51:56 +0300

    Add files via upload

---
+++ Laba_19_4.cpp
@@ -0,0 +1,41 @@
+﻿using namespace std;
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int M, N, i, j, k, c;
+	cout << "Введите количество строк:  ";
+	cin >> M;
+	cout << "Введите количество столбцов: ";
+	cin >> N;
+	float** A = new float* [M];
+	for (c = 0; c < M; c++)
+		A[c] = new float[N];
+	cout << "Введите элементы массива: " << endl;
+	for (i = 0; i < M; i++) {
+		for (j = 0; j < N; j++) {
+			cin >> A[i][j];
+		};
+	};
+	for (i = 0; i < N; i++) {
+		for (j = 1; j < N; j++) {
+			if (A[j - 1][0] > A[j][0]) {
+				for (k = 0; k < M; k++) {
+					c = A[j - 1][k];
+					A[j - 1][k] = A[j][k];
+					A[j][k] = c;
+				};
+			};
+		};
+	};
+	cout << "Измененный массив: " << endl;
+	for (i = 0; i < M; i++) {
+		for (j = 0; j < N; j++) {
+			cout << A[i][j] << "\t";
+		};
+		cout << endl;
+	};
+}
